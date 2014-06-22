2586b2c297e4c1ae391cf7ddb74141645acd41d3
katiecheng <katie.cheng@gmail.com>
2014-06-22 16:13:58 -0700

    Edited call.py such that 'called' is automatically updated with today's date

--- call.py
+++ call.py
@@ -8,6 +8,34 @@ call.py - Telemarketing script that displays the next name
           an order of over 20 Watermelons.
 
 """
+import time
+from datetime import date
+
+def get_date():
+	"A function to retrieve today's date, and return string in form MM/DD/YYYY"
+	datetime_date = date.today()
+	year_month_day = str(datetime_date).split('-')
+	today = year_month_day[1]+'/'+year_month_day[2]+'/'+year_month_day[0]
+	return today
+
+def update_csv(filename, customer_id):
+	"A function to update a customer's date-called to today's date"
+	today = get_date()
+	f = open(filename) # make a copy of the csv file
+	with open(filename, 'r+') as csvfile: # open csv file for reading/writing
+		for row in f:
+			row_list = row.split(',')
+			if row_list[0] == customer_id: # if the customer id matches
+				row_list[5]=today # update date to today's date
+				row_string = '' # initialize new row string
+				for item in row_list:
+					if item == today:
+						row_string+= str(item)+'\n' # append end of line
+					else: 
+						row_string+= str(item)+',' # append customer data
+				csvfile.write(row_string) # write updated row
+			else:
+				csvfile.write(row) # else, write copy of row
 
 # Load the customers from the passed filename
 # Return a dictionary containing the customer data
@@ -79,9 +107,12 @@ def display_customer(customer):
 	print "Name: ", customer.get('first', ''), customer.get('last', '')
 	print "Phone: ", customer.get('telephone')
 	print "\n"
+	return customer.get('customer_id', '')
+	# Added return value so the selected customer could be fed into update_csv()
 
 def main():
 	# Load data from our csv files
+	customer_file = 'customers.csv' # added filename as variable
 	customers = load_customers('customers.csv')
 	orders    = load_orders('orders.csv')
 
@@ -92,7 +123,8 @@ def main():
 			# Has this customer not been contacted yet?
 			customer = customers.get(order.get('customer_id', 0), 0)
 			if customer.get('called', '') == '':
-				display_customer(customer)
+				customer_id = display_customer(customer)
+				update_csv(customer_file, customer_id) # added call to update_csv
 				break 
 			# loop runs until it finds a customer >20 that hasn't been called
 			# shows one customer, and breaks out of for loop
